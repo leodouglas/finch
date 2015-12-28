7f75a5462797fda8a7e86cdd2c79e62147291346
Léo <leodouglas@gmail.com>
2015-12-28 12:12:42 -0500

    init

--- build/resources/test/R/nnetExample.R
+++
@@ -1,9 +0,0 @@
-require(caret)
-
-predictionNN <- predict(fitNnet.finalModel,pattern)
-predictionGLM <- predict(fitGlm.finalModel,pattern)
-prediction <- mean(c(predictionNN,predictionGLM))
-
-score <- prediction
-
-decision <- "none"
